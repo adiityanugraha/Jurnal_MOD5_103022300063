9142fdf6e7058d89c5d14dbe872338ddbacfe7ec
adiityanugraha <150656990+adiityanugraha@users.noreply.github.com>
2025-03-19 07:27:28 +0700

    Add files via upload
    menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar

---
+++ modul5_103022300063/PemrosesData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103022300063
+{
+    class PemrosesData
+    {
+        public void DapatkanNilaiTerbesar<T>(T n1, T n2, T n3)
+        {
+            dynamic nilai1 = (dynamic)n1;
+            dynamic nilai2 = (dynamic)n2;
+            dynamic nilai3 = (dynamic)n3;
+
+            if (nilai1 > nilai2 && nilai1 > nilai3)
+                {
+                    Console.WriteLine("Nilai terbesar adalah " + nilai1);
+                }
+                else if (nilai2 > nilai1 && nilai2 > nilai3)
+                {
+                    Console.WriteLine("Nilai terbesar adalah " + nilai2);
+                }
+                else if (nilai3 > nilai1 && nilai3 > nilai2)
+                {
+                    Console.WriteLine("Nilai terbesar adalah " + nilai3);
+                }
+            
+        }
+    }
+}
